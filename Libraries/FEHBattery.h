b10512fd2e42fc8c8678e372b632ab1888db2f09
Yiyang <huang.3424@osu.edu>
2020-04-18 14:06:00 -0400

    Doxygen for FEHBattery.h and FEHBuzzer.h

--- Libraries/FEHBattery.h
+++ Libraries/FEHBattery.h
@@ -3,10 +3,23 @@
 
 #include "FEHIO.h"
 
+/**
+ * @brief Access to the Proteus battery
+ * 
+ * Allows the user to check the Proteus' battery level
+ */
 class FEHBattery : public AnalogInputPin
 {
 public:
     FEHBattery(FEHIO::FEHIOPin);
+
+    /**
+     * @brief Find battery charge
+     * 
+     * Max charge is 11.2-11.7 V
+     * 
+     * @return float Voltage level
+     */
     float Voltage();
 };
 
